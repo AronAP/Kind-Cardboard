59c863e36fbbac49d952b029235c32bf2ec20ac8
AronAP <artem.prydybailo@gmail.com>
2018-12-01 02:19:29 +0200

    Add Reviews block

--- src/js/slick.optim.js
+++ src/js/slick.optim.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  
+
   $('.production-slider_for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -16,6 +16,14 @@ $(document).ready(function () {
     prevArrow: '<div class="production-arrow production-arrow_left"></div>',
     nextArrow: '<div class="production-arrow production-arrow_right"></div>',
     asNavFor: '.production-slider_for'
+  });
+
+  $('.reviews-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: '<div class="reviews-arrow reviews-arrow_left"></div>',
+    nextArrow: '<div class="reviews-arrow reviews-arrow_right"></div>'
   });
 
 });

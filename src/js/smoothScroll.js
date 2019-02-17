781dfa4a50f877044810a56013b7bf2bce094814
AronAP <artem.prydybailo@gmail.com>
2019-02-18 01:42:07 +0200

    Adaptive menu for mobile devices

--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -1,7 +1,19 @@
-function slowScroll(id) {
-  var offset = 56;
-  $('html, body').animate({
-    scrollTop: $(id).offset().top - offset
-  }, 800);
-  return false;
-}
+  if (document.documentElement.clientWidth > 768) {
+    function slowScroll(id) {
+      var offset = 56;
+      $('html, body').animate({
+        scrollTop: $(id).offset().top - offset
+      }, 800);
+      return false;
+    }
+  }
+
+  if (document.documentElement.clientWidth < 576) {
+    function slowScroll(id) {
+      var offset = -1;
+      $('html, body').animate({
+        scrollTop: $(id).offset().top - offset
+      }, 800);
+      return false;
+    }
+  }

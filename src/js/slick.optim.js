0522702586b81042d54bd2acb2e892b6be898c57
AronAP <artem.prydybailo@gmail.com>
2019-02-26 21:18:15 +0200

    Adapted the website for the mobile phones

--- src/js/slick.optim.js
+++ src/js/slick.optim.js
@@ -9,7 +9,24 @@ $(document).ready(function () {
     autoplay: true,
     draggable: false,
     autoplaySpeed: 5000,
-    asNavFor: '.production-slider_nav'
+    asNavFor: '.production-slider_nav',
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          draggable: true,
+          infinite: true,
+          dots: true,
+          fade: false,
+          autoplay: true,
+          prevArrow: '<div class="production-arrow production-arrow_left"></div>',
+          nextArrow: '<div class="production-arrow production-arrow_right"></div>',
+        }
+      },
+    ]
   });
 
   $('.production-slider_nav').slick({
@@ -29,7 +46,19 @@ $(document).ready(function () {
     slidesToScroll: 1,
     dots: false,
     prevArrow: '<div class="reviews-arrow reviews-arrow_left"></div>',
-    nextArrow: '<div class="reviews-arrow reviews-arrow_right"></div>'
+    nextArrow: '<div class="reviews-arrow reviews-arrow_right"></div>',
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          draggable: true,
+          infinite: true,
+        }
+      },
+    ]
   });
 
   //Slider for Main block
